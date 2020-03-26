5cae8fd48a4b287b482de786e7d86f4c715790d9
hoon-e <32868349+hoon-e@users.noreply.github.com>
2020-03-26 18:22:34 +0900

    3/26 study

---
+++ src/bsearch/N10816.java
@@ -0,0 +1,21 @@
+package bsearch;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class N10816 {
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        
+    }
+}
