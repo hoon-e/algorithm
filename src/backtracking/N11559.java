cc67cbe76fd05642ab8968b3e0a230713d962048
hoon-e <32868349+hoon-e@users.noreply.github.com>
2020-03-30 23:30:04 +0900

    3/30 study

--- src/backtracking/N11559.java
+++ src/backtracking/N11559.java
@@ -1,10 +1,9 @@
 package backtracking;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
+// 하나의 연쇄가 끝나기 전까지는 count를 증가시켜주면 안된다.
 public class N11559 {
     static class coord {
         int x;
@@ -20,66 +19,78 @@ public class N11559 {
     static boolean[][] chk = new boolean[12][6];
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int[] h;
-    static int[] c;
     static int score;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<12; i++){
+        for (int i = 0; i < 12; i++) {
             puyo[i] = br.readLine().toCharArray();
         }
 
-        for(int i=11; i>=0; i--){
-            for(int j=0; j<6; j++){
-                if(chk[i][j] || puyo[i][j] == '.') continue;
+        Queue<coord> q = new LinkedList<>();
+        Queue<coord> pos = new LinkedList<>();
 
-                Stack<coord> s = new Stack<>();
-                h = new int[6];
-                c = new int[6];
-                char now = puyo[i][j];
-                chk[i][j] = true;
+        boolean isAny;
+        while (true) {
+            isAny = true;
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 6; j++) {
+                    if (chk[i][j] || !Character.isAlphabetic(puyo[i][j])) continue;
 
-                Queue<coord> q = new LinkedList<>();
-                q.offer(new coord(i,j));
-                s.add(new coord(i,j));
+                    chk[i][j] = true;
+                    char now = puyo[i][j];
 
-                int count = 1;
-                while(!q.isEmpty()){
-                    coord cur = q.poll();
+                    q.clear();
+                    pos.clear();
+                    q.offer(new coord(i, j));
 
-                    for(int k=0; k<4; k++){
-                        int nx = cur.x + dx[k];
-                        int ny = cur.y + dy[k];
+                    while (!q.isEmpty()) {
+                        coord cur = q.poll();
+                        pos.offer(cur);
+                        for (int k = 0; k < 4; k++) {
+                            int nx = cur.x + dx[k];
+                            int ny = cur.y + dy[k];
 
-                        if(nx < 0 || nx >= 12 || ny < 0 || ny >= 6) continue;
-                        if(puyo[nx][ny] != 'x' && puyo[nx][ny] != now || chk[nx][ny]) continue;
-                        chk[nx][ny] = true;
-                        count++;
-                        q.offer(new coord(nx, ny));
-                        s.add(new coord(i,j));
-                    }
-                }
-
-                if(count >= 4){
-                    score++;
-
-                    while(!s.isEmpty()){
-                        coord cur = s.pop();
-                        chk[cur.x][cur.y] = false;
-                        h[cur.y]++;
-                        if( cur.x > c[cur.y] )
-                            c[cur.y] = cur.x;
+                            if (nx < 0 || nx >= 12 || ny < 0 || ny >= 6) continue;
+                            if (puyo[nx][ny] != now || chk[nx][ny]) continue;
+                            chk[nx][ny] = true;
+                            q.offer(new coord(nx, ny));
+                        }
                     }
 
-                    for(int g=0; g<6; g++){
-                       if(h[g] == 0) continue;
-
-                       for(int )
+                    if (pos.size() >= 4) {
+                        while (!pos.isEmpty()) {
+                            coord cur = pos.poll();
+                            puyo[cur.x][cur.y] = 'x';
+                        }
+                        isAny = false;
                     }
                 }
             }
+
+            for(int i=0; i<12; i++){
+                for(int j=0; j<6; j++){
+                    if(puyo[i][j] == 'x'){
+                        deleteRow(new coord(i, j));
+                    }
+                    chk[i][j] = false;
+                }
+            }
+
+            if(isAny) break;
+            score++;
         }
+
+        bw.write(String.valueOf(score));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void deleteRow(coord pos){
+        for(int i=pos.x; i>=1; i--)
+            puyo[i][pos.y] = puyo[i-1][pos.y];
+        puyo[0][pos.y] = '.';
     }
 }
