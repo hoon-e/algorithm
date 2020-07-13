6b30a13517f9195256d206c72e28159f7316f72e
hoon-e <32868349+hoon-e@users.noreply.github.com>
2020-07-14 00:00:45 +0900

    7/13 sub

--- src/RE/simulation/n14449.java
+++ src/RE/simulation/n14449.java
@@ -13,6 +13,9 @@ public class n14449 {
     // 동, 서, 북, 남
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
+    static int[] dice = {0, 0, 0, 0, 0, 0};
+    static int[] tempDice;
+
     public static void main( String[] args ) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -40,11 +43,90 @@ public class n14449 {
             int dir = Integer.parseInt(st.nextToken());
             // 전개도로 어떻게 표현할 것인가?
             // 1 : 동, 2 : 서, 3 : 북, 4 : 남
+            int nx = x + dx[dir-1];
+            int ny = y + dy[dir-1];
 
+            if(nx < 0 || nx >= N || ny < 0 || ny >= M ) continue;
+
+            x = nx;
+            y = ny;
+            tempDice = new int[6];
+
+            switch (dir) {
+                case 1:
+                    turnRight();
+                    break;
+                case 2:
+                    turnLeft();
+                    break;
+                case 3:
+                    turnBack();
+                    break;
+                case 4:
+                    turnFront();
+                    break;
+            }
+
+            out.write(dice[1]+"\n");
         }
 
         out.flush();
         in.close();
         out.close();
+    }
+
+    public static void setFloor() {
+        if(map[x][y] != 0) {
+            tempDice[3] = map[x][y];
+            map[x][y] = 0;
+        }else {
+            map[x][y] = tempDice[3];
+        }
+
+        dice = tempDice;
+    }
+
+    public static void turnFront() {
+        tempDice[0] = dice[1];
+        tempDice[1] = dice[2];
+        tempDice[2] = dice[3];
+        tempDice[3] = dice[0];
+        tempDice[4] = dice[4];
+        tempDice[5] = dice[5];
+
+        setFloor();
+    }
+
+    public static void turnLeft() {
+        tempDice[0] = dice[0];
+        tempDice[1] = dice[5];
+        tempDice[2] = dice[2];
+        tempDice[3] = dice[4];
+        tempDice[4] = dice[1];
+        tempDice[5] = dice[3];
+
+        setFloor();
+    }
+
+    public static void turnRight() {
+        tempDice[0] = dice[0];
+        tempDice[1] = dice[4];
+        tempDice[2] = dice[2];
+        tempDice[3] = dice[5];
+        tempDice[4] = dice[3];
+        tempDice[5] = dice[1];
+
+        setFloor();
+    }
+
+    public static void turnBack() {
+        tempDice[0] = dice[3];
+        tempDice[1] = dice[0];
+        tempDice[2] = dice[1];
+        tempDice[3] = dice[2];
+        tempDice[4] = dice[4];
+        tempDice[5] = dice[5];
+
+        setFloor();
     }
 }
